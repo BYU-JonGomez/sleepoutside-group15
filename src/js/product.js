f990ca52da4ff1b37ab9cf475a409449b8cfc526
NatalioMaciel <mac24026@byui.edu>
2025-09-12 23:44:40 -0300

    W02 Team Activity: Dynamic Product Detail Page

--- src/js/product.js
+++ src/js/product.js
@@ -1,31 +1,9 @@
-import { getLocalStorage, setLocalStorage, getParam } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
-
-// PRUEBA GIT 2
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
+const productID = getParam("product");
 
-function addProductToCart(product) {
-  // get current cart or empty array
-  let cart = getLocalStorage("so-cart") || [];
-
-  // add new product
-  cart.push(product);
-
-  // save updated cart
-  setLocalStorage("so-cart", cart);
-}
-
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
-
-  // get product id from query string
-  const productId = getParam("product");
+const product = new ProductDetails(productID, dataSource);
+product.init();
