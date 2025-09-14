3c0b1ad749fa46548c5cf376da0491bbad0a7603
NatalioMaciel <mac24026@byui.edu>
2025-09-13 22:35:17 -0300

    add a new folder public and put inside the folder json and images

--- src/js/product.js
+++ src/js/product.js
@@ -2,15 +2,8 @@ import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
-// 1. Obtener el productId de la URL (?product=xxx)
+const dataSource = new ProductData("tents");
 const productId = getParam("product");
 
-// 2. Crear la fuente de datos (ejemplo: "tents")
-const dataSource = new ProductData("tents");
-
-// 3. Crear instancia de ProductDetails
-const productDetails = new ProductDetails(productId, dataSource);
-
-// 4. Inicializar (renderizar producto + activar botón)
-productDetails.init();
-
+const product = new ProductDetails(productId, dataSource);
+product.init();
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -9,8 +9,8 @@ export function getLocalStorage(key) {
 }
 
 // save data to localStorage
-export function setLocalStorage(key, value) {
-  localStorage.setItem(key, JSON.stringify(value));
+export function setLocalStorage(key, data) {
+  localStorage.setItem(key, JSON.stringify(data));
 }
 
 // set a listener for both touchend and click
@@ -25,5 +25,15 @@ export function setClick(selector, callback) {
 export function getParam(param) {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-  return urlParams.get(param);
+  const product = urlParams.get(param);
+  return product
+}
+
+export function renderListWithTemplate(template, parentElement, list, position = "afterbegin", clear = false) {
+  const htmlStrings = list.map(template);
+  // if clear is true we need to clear out the contents of the parent.
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
 }
