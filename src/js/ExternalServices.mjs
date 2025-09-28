5370c2a3401e4ac2630412ffd21f5367c2d7aa20
NatalioMaciel <mac24026@byui.edu>
2025-09-27 22:21:11 -0300

    finished the part 5 and part 6

--- src/js/ExternalServices.mjs
+++ src/js/ExternalServices.mjs
@@ -13,12 +13,6 @@ export default class ExternalServices {
     // The category is passed in to the getData method
   }
 
-  getData() {
-    return fetch(this.path)
-      .then(convertToJson)
-      .then((data) => data);
-  }
-
   // Fetch and return the full product data array
   async getData(category) {
     const response = await fetch(`${baseURL}products/search/${category}`);
@@ -30,6 +24,18 @@ export default class ExternalServices {
   async findProductById(id) {
     const response = await fetch(`${baseURL}product/${id}`);
     const data = await convertToJson(response);
+    console.log(data.Result);
     return data.Result;
+  }
+
+  async checkout(payload) {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    };
+    return await fetch(`${baseURL}checkout/`, options).then(convertToJson);
   }
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,3 @@
-import ExternalServices from "./ExternalServices.mjs";
-import ProductList from "./ProductList.mjs";
-import { qs } from "./utils.mjs";
-
-const ExternalServices = new ExternalServices("tents");
-const productListElement = qs(".product-list");
-
-const productList = new ProductList("tents", ExternalServices, productListElement);
-productList.init();
 import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
