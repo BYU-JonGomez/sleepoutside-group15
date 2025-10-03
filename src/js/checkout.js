d801e52c1fd13222bdf61aa04c399e8f952929c4
NatalioMaciel <mac24026@byui.edu>
2025-10-03 20:37:44 -0300

    individual activity week04

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -8,29 +8,29 @@ checkoutProcess.init();
 
 const form = document.querySelector("#checkout-form");
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    checkoutProcess.checkout(form);
+  e.preventDefault();
+  checkoutProcess.checkout(form);
 });
 
 // Input formatting
 const cardNumberInput = document.querySelector("#cardNumber");
 cardNumberInput.addEventListener("input", (e) => {
-    let value = e.target.value.replace(/\D/g, ""); // Remove non-digits
-    let formattedValue = "";
-    for (let i = 0; i < value.length; i++) {
-        if (i > 0 && i % 4 === 0) {
-            formattedValue += "-";
-        }
-        formattedValue += value[i];
+  let value = e.target.value.replace(/\D/g, ""); // Remove non-digits
+  let formattedValue = "";
+  for (let i = 0; i < value.length; i++) {
+    if (i > 0 && i % 4 === 0) {
+      formattedValue += "-";
     }
-    e.target.value = formattedValue;
+    formattedValue += value[i];
+  }
+  e.target.value = formattedValue;
 });
 
 const expirationInput = document.querySelector("#expiration");
 expirationInput.addEventListener("input", (e) => {
-    let value = e.target.value.replace(/\D/g, ""); // Remove non-digits
-    if (value.length > 2) {
-        value = value.slice(0, 2) + "/" + value.slice(2);
-    }
-    e.target.value = value;
+  let value = e.target.value.replace(/\D/g, ""); // Remove non-digits
+  if (value.length > 2) {
+    value = value.slice(0, 2) + "/" + value.slice(2);
+  }
+  e.target.value = value;
 });
