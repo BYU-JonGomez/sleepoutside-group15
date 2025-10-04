05b8c0128f7ea56d77ba7d1a1133d8b904c2637d
altoledo27 <altoledoz27@gmail.com>
2025-10-03 19:43:58 -0600

    Remove from cart feature w03

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,4 +1,8 @@
-import { getLocalStorage, loadHeaderFooter } from "./utils.mjs";
+import {
+  getLocalStorage,
+  setLocalStorage,
+  loadHeaderFooter,
+} from "./utils.mjs";
 
 loadHeaderFooter().then(() => {
   const cartItems = JSON.parse(localStorage.getItem("so-cart")) || [];
@@ -10,18 +14,25 @@ const divRef = document.querySelector(".price-div");
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
-  if (cartItems) {
+
+  if (cartItems && cartItems.length > 0) {
     const htmlItems = cartItems.map((item) => cartItemTemplate(item));
     document.querySelector(".product-list").innerHTML = htmlItems.join("");
 
+    document.querySelectorAll(".cart-remove-item").forEach((btn) => {
+      btn.addEventListener("click", removeFromCart);
+    });
+
     renderTotal(cartItems);
+    updateCartCount(cartItems.length);
   } else {
-    divRef.setAttribute("class", `hidden`);
+    divRef.setAttribute("class", "hidden");
+    document.querySelector(".product-list").innerHTML = "";
+    updateCartCount(0);
   }
-  updateCartCount(cartItems.length);
 }
 
-function cartItemTemplate(item) {
+function cartItemTemplate(item, index) {
   const imgSrc = item.Images ? item.Images.PrimaryMedium : item.Image;
   const newItem = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
@@ -36,6 +47,7 @@ function cartItemTemplate(item) {
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">${item.FinalPrice}</p>
+  <button class="cart-remove-item" data-index="${index}">❌</button>
 </li>`;
 
   return newItem;
@@ -48,6 +60,17 @@ function renderTotal(cartItems) {
   });
 
   priceContainer.textContent = `Total: $${sumPrices.toFixed(2)}`;
+}
+
+function removeFromCart(event) {
+  const index = event.target.dataset.index;
+  let cartItems = getLocalStorage("so-cart") || [];
+
+  cartItems.splice(index, 1);
+
+  setLocalStorage("so-cart", cartItems);
+
+  renderCartContents();
 }
 
 export function updateCartCount(count) {
